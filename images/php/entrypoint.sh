d5caf7bb089d9eba5f55f8ca9587a95e97484b5b
oageev <ageev.oleg.v@gmail.com>
2020-06-22 19:11:00 +0300

    docker stuff

--- images/php/entrypoint.sh
+++ images/php/entrypoint.sh
@@ -1,4 +1,4 @@
-chown -R www-data /var/www/storage
+chown -R www-data /var/www/storage /var/www/bootstrap/cache
 cp /var/www/.env.dev /var/www/.env
 chown www-data /var/www/.env
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
